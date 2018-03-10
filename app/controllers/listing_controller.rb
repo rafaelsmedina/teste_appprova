100658774776051f3e9ebbb2c763439423e0abad
rafaelsmedina <r.salesmf@gmail.com>
2018-03-10 06:09:48 -0300

    Adds average grade filter

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,26 +1,48 @@
 class ListingController < ApplicationController
   def index
   	@courses = Course.joins(:institution).order('institutions.grade DESC')
+  	@groups = Student.group(:course_id).average(:grade)
   end
 
   def institution
   	param = params[:id]
   	@courses = Course.joins(:institution).where(institutions: {id: param}).order('institutions.grade DESC')
+  	@groups = Student.group(:course_id).average(:grade)
   end
 
   def course
   	param = params[:id]
   	@courses = Course.joins(:institution).where(courses: {name: param}).order('institutions.grade DESC')
+  	@groups = Student.group(:course_id).average(:grade)
   end
 
   def institution_grade
   	param = params[:id]
   	@courses = Course.joins(:institution).where(institutions: {grade: param}).order('institutions.grade DESC')
+  	@groups = Student.group(:course_id).average(:grade)
   end
 
   def course_grade
   	param = params[:id]
   	@courses = Course.joins(:institution).where(courses: {grade: param}).order('institutions.grade DESC')
+  	@groups = Student.group(:course_id).average(:grade)
   end
 
+  def average_grade
+  	param = params[:id]
+  	
+  	@groups = Student.group(:course_id).average(:grade)
+
+  	array = []
+
+  	@groups.each do |group|
+  		puts 'oi', group[1].to_i, param
+  		if group[1].to_i == param.to_i
+  			puts 'entray'
+  			array.push(group[0])
+  		end
+  	end
+
+  	@courses = Course.joins(:institution).where(courses: {id: array}).order('institutions.grade DESC')
+  end
 end
