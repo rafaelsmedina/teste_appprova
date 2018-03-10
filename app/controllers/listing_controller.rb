c964a80e034298fcaed98ead3cac1fb0e57ac9e4
rafaelsmedina <r.salesmf@gmail.com>
2018-03-10 04:35:44 -0300

    Adds filters to index page

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,4 +1,26 @@
 class ListingController < ApplicationController
   def index
+  	@courses = Course.joins(:institution).order('institutions.grade DESC')
   end
+
+  def institution
+  	param = params[:id]
+  	@courses = Course.joins(:institution).where(institutions: {id: param}).order('institutions.grade DESC')
+  end
+
+  def course
+  	param = params[:id]
+  	@courses = Course.joins(:institution).where(courses: {name: param}).order('institutions.grade DESC')
+  end
+
+  def institution_grade
+  	param = params[:id]
+  	@courses = Course.joins(:institution).where(institutions: {grade: param}).order('institutions.grade DESC')
+  end
+
+  def course_grade
+  	param = params[:id]
+  	@courses = Course.joins(:institution).where(courses: {grade: param}).order('institutions.grade DESC')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'listing/index'
+  get 'listing/institution/:id' => 'listing#institution', as: 'listing_institution'
+  get 'listing/course/:id' => 'listing#course', as: 'listing_course'
+  get 'listing/institution_grade/:id' => 'listing#institution_grade', as: 'listing_institution_grade'
+  get 'listing/course_grade/:id' => 'listing#course_grade', as: 'listing_course_grade'
+  get 'listing/average_grade/:id' => 'listing#average_grade', as: 'listing_average_grade'
 
   resources :students
   resources :courses
