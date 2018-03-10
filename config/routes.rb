4f94ef5fbb6aa639c978f607cd61250562db8983
rafaelsmedina <r.salesmf@gmail.com>
2018-03-10 00:36:28 -0300

    Adds listings controller and makes it root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'listing/index'
+
   resources :students
   resources :courses
   resources :institutions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'listing#index'
 end
